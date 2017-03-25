42dc1de9af881409bc0414a97dffab0199532770
kdr35 <kdr35@pitt.edu>
2017-03-25 13:32:48 -0400

    test

--- src/main/java/edu/infsci2560/services/DocumentCategoriesService.java
+++ src/main/java/edu/infsci2560/services/DocumentCategoriesService.java
@@ -10,6 +10,8 @@ import edu.infsci2560.repositories.DocumentCategoryRepository;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import static com.google.common.collect.Iterators.forEnumeration;
+import static com.google.common.collect.Lists.newArrayList;
 import java.util.concurrent.atomic.AtomicInteger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
