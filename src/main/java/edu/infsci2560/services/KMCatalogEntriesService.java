5cbbe218aba4bfe0be7973fb466714718d34f6c6
krodgers351 <krodgers351@users.noreply.github.com>
2019-06-04 17:58:51 -0400

    Update KMCatalogEntriesService.java

--- src/main/java/edu/infsci2560/services/KMCatalogEntriesService.java
+++ src/main/java/edu/infsci2560/services/KMCatalogEntriesService.java
@@ -26,7 +26,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
- *
+ * KMCatalogEntriesService
  * @author KRodgers
  */
 @RestController
